17e61706692b6e9c52c5157c5827e960656e1e1d
webopsway <webmaster@opsway.fr>
2025-07-17 04:44:12 +0200

    update

--- src/components/documentation/extensions/DraggableParagraphNodeView.tsx
+++ src/components/documentation/extensions/DraggableParagraphNodeView.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { NodeViewWrapper, NodeViewContent, ReactNodeViewProps } from '@tiptap/react';
 import { BlockDragHandle } from './BlockDragHandle';
+import { useBlockDnd } from './BlockDndExtension';
 
 export const DraggableParagraphNodeView: React.FC<ReactNodeViewProps> = (props) => {
-  // TODO: brancher les listeners dnd-kit ici
+  const nodeId = props.node?.attrs?.id || props.node?.attrs?.name || props.getPos?.();
+  const { getDragProps, getDropProps, isDraggingId } = useBlockDnd();
+  const dragProps = getDragProps(String(nodeId));
+  const dropProps = getDropProps(String(nodeId)) || {};
+
   return (
-    <NodeViewWrapper {...props} className="flex items-center group hover:bg-muted/30 rounded px-1 py-0.5">
+    <NodeViewWrapper
+      ref={typeof dropProps.setNodeRef === 'function' ? dropProps.setNodeRef : undefined}
+      className={`flex items-center group hover:bg-muted/30 rounded px-1 py-0.5 ${isDraggingId === String(nodeId) ? 'opacity-50' : ''}`}
+      data-node-id={nodeId}
+    >
       <div className="mr-2 opacity-0 group-hover:opacity-100 transition-opacity">
-        <BlockDragHandle />
+        <BlockDragHandle {...dragProps} />
       </div>
       <div className="flex-1">
         <NodeViewContent as="div" className="outline-none min-h-[1.5rem]" />
