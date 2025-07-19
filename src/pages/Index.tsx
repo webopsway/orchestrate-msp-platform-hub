8a46fab7202df1bacdad4e9b056d5d638a7f09fd
Steeve CLOTILDE <steeve@clotilde.dev>
2025-07-19 19:08:03 +0200

    update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,7 @@
 import Dashboard from "./Dashboard";
-import { RoleAdmin } from "@/components/admin/RoleAdmin";
-import { useAuth } from "@/contexts/AuthContext";
 
 const Index = () => {
-  const { userProfile } = useAuth();
-  const isMspAdmin = userProfile?.is_msp_admin || false;
-
-  return (
-    <div className="space-y-6">
-      {isMspAdmin && <RoleAdmin />}
-      <Dashboard />
-    </div>
-  );
+  return <Dashboard />;
 };
 
 export default Index;
